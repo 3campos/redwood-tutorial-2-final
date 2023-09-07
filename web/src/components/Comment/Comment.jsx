2034666116dbc4c675d87ce74f35ab77d6443417
ecampos <ecampos.jr54@gmail.com>
2023-09-06 20:20:06 -0500

    add comme nt structure for users to comment, i.e., name, date, body

--- web/src/components/Comment/Comment.jsx
+++ web/src/components/Comment/Comment.jsx
@@ -1,8 +1,10 @@
-const Comment = () => {
+const Comment = ({ comment }) => {
   return (
     <div>
-      <h2>{'Comment'}</h2>
+      <h2>{comment.name}</h2>
+      <time dateTime={comment.createdAt}>{comment.createdAt}</time>
       <p>{'Find me in ./web/src/components/Comment/Comment.jsx'}</p>
+      <p>{comment.body}</p>
     </div>
   )
 }
